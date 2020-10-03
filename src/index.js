5273fee35a0f902c33c2b6b113c5edfe9c5f6494
nigmatulin8 <rustam280897@gmail.com>
2020-10-03 18:12:02 +0400

    feat:add basic func

--- src/index.js
+++ src/index.js
@@ -2,24 +2,38 @@ exports.min = function min(arr) {
     if (!arr || arr.length == 0) {
         return 0;
     }
-    return Math.min(...arr);
-}
 
+    let min = Number.POSITIVE_INFINITY;
+
+    arr.forEach(item => {
+        if(item < min) {
+            min = item;
+        }
+    });
+
+    return min;
+}
 
 exports.max = function max(arr) {
     if (!arr || arr.length == 0) {
         return 0;
     }
-    return Math.max(...arr);
+
+    let max = Number.NEGATIVE_INFINITY;
+
+    arr.forEach(item => {
+        if(item > max) {
+            max = item;
+        }
+    });
+
+    return max;
 }
 
 exports.avg = function avg(arr) {
     if (!arr || arr.length == 0) {
         return 0;
     }
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += Number(arr[i]);
-    }
-    return sum / arr.length;
+
+    return arr.reduce((sum, item) => sum + item) / arr.length;
 }
